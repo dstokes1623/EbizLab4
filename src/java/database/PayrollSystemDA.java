9f328de77e37505183940c5829cf1fa7d336621c
Dante Stokes <dstokes@gus.pittstate.edu>
2022-04-01 09:01:23 -0500

    first part of lab done

--- src/java/database/PayrollSystemDA.java
+++ src/java/database/PayrollSystemDA.java
@@ -1,9 +1,28 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PayrollSystemDA {
-    public static void initialize() {
-        EmployeeDA.initialize();
-        TimecardDA.initialize();
+    private static Connection connection;
+    
+    public static void initialize() throws SQLException {
+//        EmployeeDA.initialize();
+//        TimecardDA.initialize();
+        try{
+             Connection connection;
+             connection = DriverManager.getConnection("jdbc:derby://localhost:1527/PayrollSystemDB", "CIS640", "cis640");
+        } catch(Exception e){
+            System.out.println(e);
+        }
+         
         WithholdingTypeDA.initialize();
     }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+    
+    
 }
